dc55186cac27972c728ba0ec20c680152bad3136
davidmintic <davidperez.tic@ucaldas.edu.co>
2021-10-07 17:11:00 -0500

    Edit post

--- Veterinaria.App/Veterinaria.App.Presentacion/Pages/Admin/AdminVeterinario.cshtml.cs
+++ Veterinaria.App/Veterinaria.App.Presentacion/Pages/Admin/AdminVeterinario.cshtml.cs
@@ -41,6 +41,9 @@ namespace Veterinaria.App.Presentacion.Pages
             this.RefreshData();
         }
 
+
+        
+
         // asp-page-handler=""
         public  void OnPostDel(int id)
         {
@@ -54,6 +57,29 @@ namespace Veterinaria.App.Presentacion.Pages
                  Console.WriteLine("NO ENTRÓ con el id ");
             }
         }
+
+        
+        // asp-page-handler=""
+        public  IActionResult  OnPostEdit(int id)
+        {
+            if(id != null) {
+                // Console.WriteLine("entro con el id "+ id);
+                // this.RefreshData();
+                // repositorioVeterinario.Eliminar(id);
+                // this.RefreshData();
+
+        //          string url = "/.auth/login/aad?post_login_redirect_url=" 
+        //   + Request.Query["redirect_url"];
+
+                return Redirect("/index?id="+id);
+            } else {
+                this.RefreshData();
+                 Console.WriteLine("NO ENTRÓ con el id ");
+
+                 return Redirect("/index");
+            }
+        }
+
 
 
         public void RefreshData() {
